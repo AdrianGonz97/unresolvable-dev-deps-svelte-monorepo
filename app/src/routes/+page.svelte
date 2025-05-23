debce1bd4c117642057d6f63128939a77f7321e9
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2025-05-23 11:45:17 -0400

    init

---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import { Button } from "@workspace/ui/components/button/index.js";
+</script>
+
+<Button>foo</Button>
---
+++ ui/src/components/button/button.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import SomeIcon from "@lucide/svelte/icons/bubbles";
+
+	let { children } = $props();
+</script>
+
+<button>
+	<SomeIcon />
+	{@render children?.()}
+</button>
